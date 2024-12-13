1597604f277e83bc937459753fcc3bd6d90687ac
jcampeau25 <jdccanuck25@gmail.com>
2024-12-13 09:37:45 -0500

    in progress

--- Final Project 2024 - 2025/Game1.cs
+++ Final Project 2024 - 2025/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 namespace Final_Project_2024___2025
 {
@@ -9,12 +10,19 @@ namespace Final_Project_2024___2025
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        Texture2D hallwayTexture, titleBackgroundTexture, runLeftTexture, runRightTexture, idleTexture, buttonTexture;
-        Rectangle window, playButtonRect, levelButtonRect;
+        Texture2D hallwayTexture, titleBackgroundTexture, runLeftTexture1, runLeftTexture2, runLeftTexture3, runRightTexture1, runRightTexture2, runRightTexture3, idleTexture, buttonTexture, playerTexture;
+        Rectangle window, playButtonRect, levelButtonRect, playerRect;
+        Vector2 playerSpeed;
         Screen screen;
         MouseState mouseState, prevMouseState;
+        KeyboardState keyboardState;
         SpriteFont introFont, buttonFont;
 
+        int runRight = 0;
+        int runLeft = 0;
+
+        List<Texture2D> runRightTextures;
+        List<Texture2D> runLeftTextures;
         enum Screen
         {
             Title,
@@ -32,10 +40,18 @@ namespace Final_Project_2024___2025
         {
             // TODO: Add your initialization logic here
 
-            base.Initialize();
             window = new Rectangle(0, 0, 1000, 600);
             playButtonRect = new Rectangle(568, 330, 180, 75);
             levelButtonRect = new Rectangle(568, 430, 180, 75);
+            playerRect = new Rectangle(20, 350, 250, 250);
+            playerSpeed = new Vector2();
+            runRightTextures = new List<Texture2D>();
+            runLeftTextures = new List<Texture2D>();
+
+
+            base.Initialize();
+            
+
         }
 
         protected override void LoadContent()
@@ -54,22 +70,58 @@ namespace Final_Project_2024___2025
             introFont = Content.Load<SpriteFont>("introFont");
             buttonFont = Content.Load<SpriteFont>("ButtonFont");
             buttonTexture = Content.Load<Texture2D>("rectangle");
+            runRightTextures.Add(Content.Load<Texture2D>("coltonRunRight"));
+            runRightTextures.Add(Content.Load<Texture2D>("coltonRunRight2"));
+            runRightTextures.Add(Content.Load<Texture2D>("coltonRunRight3"));
+            runLeftTextures.Add(Content.Load<Texture2D>("coltonRunLeft1"));
+            runLeftTextures.Add(Content.Load<Texture2D>("coltonRunLeft2"));
+            runLeftTextures.Add(Content.Load<Texture2D>("coltonRunLeft3"));
+            playerTexture = idleTexture;
+
+
+
         }
 
         protected override void Update(GameTime gameTime)
         {
             prevMouseState = mouseState;
             mouseState = Mouse.GetState();
+            keyboardState = Keyboard.GetState();
+            playerSpeed = Vector2.Zero;
+
             this.Window.Title = $"x = {mouseState.X}, y = {mouseState.Y}";
 
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             // TODO: Add your update logic here
-            if (mouseState.LeftButton == ButtonState.Pressed && prevMouseState.LeftButton == ButtonState.Released)
+            if (screen == Screen.Title)
+                if (mouseState.LeftButton == ButtonState.Pressed && prevMouseState.LeftButton == ButtonState.Released)
+                {
+                    if (playButtonRect.Contains(mouseState.Position))
+                        screen = Screen.Level1;
+                }
+            if (screen != Screen.Title)
             {
-                if (playButtonRect.Contains(mouseState.Position))
-                    screen = Screen.Level1;
+                if (keyboardState.IsKeyDown(Keys.D))
+                {
+                    playerSpeed.X += 5;
+                    runRight += 1;
+                    if (runRight > 2)
+                        runRight = 0;
+                    playerTexture = runRightTextures[runRight];
+                }
+                if (keyboardState.IsKeyDown(Keys.A))
+                {
+                    playerSpeed.X -= 5;
+                    runLeft += 1;
+                    if (runLeft > 2)
+                        runLeft = 0;
+                    playerTexture = runLeftTextures[runLeft];
+                }
+
+                playerRect.X += (int)playerSpeed.X;
+
             }
             base.Update(gameTime);
         }
@@ -95,6 +147,7 @@ namespace Final_Project_2024___2025
             if (screen == Screen.Level1)
             {
                 _spriteBatch.Draw(hallwayTexture, window, Color.White);
+                _spriteBatch.Draw(playerTexture, playerRect, Color.White);
             }
             _spriteBatch.End();
             base.Draw(gameTime);
