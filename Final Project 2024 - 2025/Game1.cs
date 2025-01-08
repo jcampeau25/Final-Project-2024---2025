4a175d72757795fda55d325f5297791f3137d161
jcampeau25 <jdccanuck25@gmail.com>
2025-01-08 09:34:30 -0500

    in progress

--- Final Project 2024 - 2025/Game1.cs
+++ Final Project 2024 - 2025/Game1.cs
@@ -11,8 +11,10 @@ namespace Final_Project_2024___2025
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        Texture2D hallwayTexture, titleBackgroundTexture,idleTexture, buttonTexture, playerTexture,cabinetTexture, cabinetShutTexture, cabinetOpenTexture;
-        Rectangle window, playButtonRect, levelButtonRect, playerRect, inCabinetRect;
+        Texture2D hallwayTexture, titleBackgroundTexture, idleTexture, buttonTexture, playerTexture;
+        Texture2D cabinetTexture, cabinetShutTexture, cabinetOpenTexture, doorTexture, twoLayerTexture;
+        Texture2D rectangleTexture, wallTexture, guardLeftTexture, guardRightTexture, guardUpTexture,guardDownTexture;
+        Rectangle window, playButtonRect, levelButtonRect, playerRect, doorRect, guardRect;
         Vector2 playerSpeed, jumpSpeed;
         Screen screen;
         MouseState mouseState, prevMouseState;
@@ -31,11 +33,12 @@ namespace Final_Project_2024___2025
 
         List<Texture2D> runRightTextures;
         List<Texture2D> runLeftTextures;
-        List<Rectangle> cabinetRects;
+        List<Rectangle> wallRects;
         enum Screen
         {
             Title,
             LevelSelect,
+            Tutorial,
             Level1
         }
         public Game1()
@@ -52,14 +55,12 @@ namespace Final_Project_2024___2025
             window = new Rectangle(0, 0, 1000, 600);
             playButtonRect = new Rectangle(568, 330, 180, 75);
             levelButtonRect = new Rectangle(568, 430, 180, 75);
-            playerRect = new Rectangle(20, 350, 250, 250);
-            inCabinetRect = new Rectangle(10000, 10000, 1, 1);
+            playerRect = new Rectangle(20, 500, 100, 100);
             playerSpeed = new Vector2();
             runRightTextures = new List<Texture2D>();
             runLeftTextures = new List<Texture2D>();
-            cabinetRects = new List<Rectangle>();
-
-            cabinetRects.Add(new Rectangle(700, 305, 150, 200));
+            doorRect = new Rectangle(875, 450, 110, 180);
+            guardRect = new Rectangle(430, 200, 75, 85);
 
             base.Initialize();
             
@@ -77,22 +78,31 @@ namespace Final_Project_2024___2025
             _graphics.ApplyChanges();
 
             idleTexture = Content.Load<Texture2D>("Colton Prisoner");
+            rectangleTexture = Content.Load<Texture2D>("Rectangle");
             titleBackgroundTexture = Content.Load<Texture2D>("titleBackground");
             hallwayTexture = Content.Load<Texture2D>("prison background");
             introFont = Content.Load<SpriteFont>("introFont");
             buttonFont = Content.Load<SpriteFont>("ButtonFont");
             buttonTexture = Content.Load<Texture2D>("rectangle");
+
             runRightTextures.Add(Content.Load<Texture2D>("coltonRunRight"));
             runRightTextures.Add(Content.Load<Texture2D>("coltonRunRight2"));
             runRightTextures.Add(Content.Load<Texture2D>("coltonRunRight3"));
             runLeftTextures.Add(Content.Load<Texture2D>("coltonRunLeft1"));
             runLeftTextures.Add(Content.Load<Texture2D>("coltonRunLeft2"));
             runLeftTextures.Add(Content.Load<Texture2D>("coltonRunLeft3"));
+
+            guardLeftTexture = Content.Load<Texture2D>("guard left");
+            guardRightTexture = Content.Load<Texture2D>("guard right");
+            guardUpTexture = Content.Load<Texture2D>("guard up");
+            guardDownTexture = Content.Load<Texture2D>("guard down");
+
+
+            wallTexture = Content.Load<Texture2D>("brick wall");
             cabinetShutTexture = Content.Load<Texture2D>("cabinet shut");
             cabinetOpenTexture = Content.Load<Texture2D>("cabinet open");
-            creak = Content.Load<SoundEffect>("cabinet creak");
-            creakInstance = creak.CreateInstance();
-            cabinetTexture = cabinetShutTexture;
+            twoLayerTexture = Content.Load<Texture2D>("2 layer jail");
+            doorTexture = Content.Load<Texture2D>("prison door");
             playerTexture = idleTexture;
 
 
@@ -117,90 +127,55 @@ namespace Final_Project_2024___2025
                 if (mouseState.LeftButton == ButtonState.Pressed && prevMouseState.LeftButton == ButtonState.Released)
                 {
                     if (playButtonRect.Contains(mouseState.Position))
-                        screen = Screen.Level1;
+                        screen = Screen.Tutorial;
                 }
-            if (screen != Screen.Title)
+            if (screen == Screen.Tutorial)
             {
-                if (playerRect.Bottom >= 600)
-                    onGround = true;
-
-                if (playerRect.Bottom < 600)
-                    onGround = false;
-
-                if (keyboardState.IsKeyDown(Keys.D))
-                {
-                    playerSpeed.X += 7;
-                    runSeconds += (float)gameTime.ElapsedGameTime.TotalSeconds;
-                    if (runSeconds >= 0.25)
-                    {
-                        runSeconds = 0;
-                        runRight += 1;
-                    }
-                    if (runRight > 2)
-                        runRight = 0;
-                    playerTexture = runRightTextures[runRight];
-                }
-                if (keyboardState.IsKeyDown(Keys.A))
-                {
-                    playerSpeed.X -= 7;
-                    runSeconds += (float)gameTime.ElapsedGameTime.TotalSeconds;
-                    if (runSeconds >= 0.25)
-                    {
-                        runSeconds = 0;
-                        runLeft += 1;
-                    }
-
-                    if (runLeft > 2)
-                        runLeft = 0;
-                    playerTexture = runLeftTextures[runLeft];
-                }
-
-                if (keyboardState.IsKeyUp(Keys.A) && keyboardState.IsKeyUp(Keys.D))
-                    playerTexture = idleTexture;
-
-
-                if (onGround == true)
-                {
                     if (keyboardState.IsKeyDown(Keys.W))
+                        playerSpeed.Y -= 7;
+
+                    if (keyboardState.IsKeyDown(Keys.S))
+                        playerSpeed.Y += 7;
+
+
+                
+                    if (playerRect.Bottom >= 600)
+                        onGround = true;
+
+                    if (playerRect.Bottom < 600)
+                        onGround = false;
+
+                    if (keyboardState.IsKeyDown(Keys.D))
                     {
-                        jump = true;
-                    }
-                   
-                }
-
-                if (jump == true)
-                    playerSpeed.Y -= 7;
-
-                if (playerRect.Top <= 150)
-                {
-                    jump = false;
-
-                }
-
-                if (jump == false && playerRect.Bottom < 600)
-                    playerSpeed.Y += 7;
-
-                foreach (Rectangle cabinet in cabinetRects)
-                {
-                    if (playerRect.Intersects(cabinet) || playerRect.X > 9000)
-                        if (keyboardState.IsKeyDown(Keys.E))
+                        playerSpeed.X += 7;
+                        runSeconds += (float)gameTime.ElapsedGameTime.TotalSeconds;
+                        if (runSeconds >= 0.25)
                         {
-                            cabinetTexture = cabinetOpenTexture;
-                            creakInstance.Play();
-                            inCabinet = !inCabinet;
-                            
+                            runSeconds = 0;
+                            runRight += 1;
+                        }
+                        if (runRight > 2)
+                            runRight = 0;
+                        playerTexture = runRightTextures[runRight];
+                    }
+                    if (keyboardState.IsKeyDown(Keys.A))
+                    {
+                        playerSpeed.X -= 7;
+                        runSeconds += (float)gameTime.ElapsedGameTime.TotalSeconds;
+                        if (runSeconds >= 0.25)
+                        {
+                            runSeconds = 0;
+                            runLeft += 1;
                         }
 
+                        if (runLeft > 2)
+                            runLeft = 0;
+                        playerTexture = runLeftTextures[runLeft];
+                    }
 
-                }
+                    if (keyboardState.IsKeyUp(Keys.A) && keyboardState.IsKeyUp(Keys.D))
+                        playerTexture = idleTexture;
 
-                if (inCabinet == true && creakInstance.State == SoundState.Stopped)
-                {
-                    playerRect = inCabinetRect;
-                    cabinetTexture = cabinetShutTexture;
-                }
-                if (inCabinet == false && creakInstance.State == SoundState.Playing)
-                    playerRect = new Rectangle(cabinetRects[0].X, 350, 250, 250);
 
                 playerRect.X += (int)playerSpeed.X;
                 playerRect.Y += (int)playerSpeed.Y;
@@ -223,19 +198,20 @@ namespace Final_Project_2024___2025
                 _spriteBatch.DrawString(introFont, ("Jailhouse 2"), new Vector2(500, 200), Color.Red);
                 _spriteBatch.Draw(buttonTexture, playButtonRect, Color.CornflowerBlue);
                 _spriteBatch.Draw(buttonTexture, levelButtonRect, Color.CornflowerBlue);
-                _spriteBatch.DrawString(buttonFont, ("PLAY"), new Vector2(605, 345), Color.Red);
+                _spriteBatch.DrawString(buttonFont, ("LEARN"), new Vector2(590, 345), Color.Red);
                 _spriteBatch.DrawString(buttonFont, ("LEVELS"), new Vector2(580, 445), Color.Red);
             }
 
-            if (screen == Screen.Level1)
+            if (screen == Screen.Tutorial)
             {
-                _spriteBatch.Draw(hallwayTexture, window, Color.White);
-                _spriteBatch.DrawString(buttonFont, ("A/D To Move"), new Vector2(60, 60), Color.Red);
-                _spriteBatch.DrawString(buttonFont, ("W To Jump"), new Vector2(380, 60), Color.Red);
-                _spriteBatch.DrawString(buttonFont, ("E to Interact"), new Vector2(670, 60), Color.Red);
-                _spriteBatch.Draw(cabinetTexture, cabinetRects[0], Color.White);
-
+                _spriteBatch.Draw(rectangleTexture, window, Color.Black);
+                _spriteBatch.DrawString(buttonFont, ("W/A/S/D To Move"), new Vector2(60, 10), Color.Red);
+                _spriteBatch.DrawString(buttonFont, ("Get To The Door"), new Vector2(550, 10), Color.Red);
+                _spriteBatch.Draw(doorTexture, doorRect, Color.White);
                 _spriteBatch.Draw(playerTexture, playerRect, Color.White);
+                _spriteBatch.Draw(guardDownTexture, guardRect, Color.White);
+                _spriteBatch.Draw(wallTexture, new Rectangle(0, 470, 700, 20), Color.White);
+                _spriteBatch.Draw(wallTexture, new Rectangle(850, 340, 20, 260), Color.White);
 
             }
             _spriteBatch.End();
